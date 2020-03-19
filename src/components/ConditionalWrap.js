53e3d587756c2fc5eb3c38b7b1a3999e5060bb3e
Matt Layton <mattlayt@gmail.com>
2020-03-19 08:44:01 +0000

    2.0

--- src/components/ConditionalWrap.js
+++
@@ -1,5 +0,0 @@
-// credit to https://github.com/kitze/conditional-wrap
-const ConditionalWrap = ({ condition, wrap, children }) =>
-  condition ? wrap(children) : children;
-
-export default ConditionalWrap;
